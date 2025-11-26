15c46968ff8dd11c0d7780397af7577901690325
scout <h3ct0r.ml@gmail.com>
2025-11-26 16:10:57 -0300

    added basic packages to generate LiDAR maps

--- ros2_packages/scout_init/launch/msg_MID360_launch.py
+++ ros2_packages/scout_init/launch/msg_MID360_launch.py
@@ -5,7 +5,7 @@ from launch_ros.actions import Node
 import launch
 
 ################### user configure parameters for ros2 start ###################
-xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
+xfer_format   = 1    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
 multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar, others-Invalid data src
 publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
@@ -16,7 +16,6 @@ cmdline_bd_code = 'livox0000000001'
 
 cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
 cur_config_path = cur_path + '../config'
-rviz_config_path = os.path.join(cur_config_path, 'display_point_cloud_ROS2.rviz')
 user_config_path = os.path.join(cur_config_path, 'MID360_config.json')
 ################### user configure parameters for ros2 end #####################
 
@@ -42,25 +41,8 @@ def generate_launch_description():
         parameters=livox_ros2_params
         )
 
-    livox_rviz = Node(
-            package='rviz2',
-            executable='rviz2',
-            output='screen',
-            arguments=['--display-config', rviz_config_path]
-        )
-    
-    static_tf = Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            # Arguments for x, y, z, roll, pitch, yaw, and frame names
-            # Example: 1 meter offset in z, 0 rotation, between 'world' and 'mystaticturtle'
-            arguments=['0', '0', '0', '0', '0', '0', 'camera_init', frame_id]
-        )
-
     return LaunchDescription([
-        static_tf,
         livox_driver,
-        livox_rviz,
         # launch.actions.RegisterEventHandler(
         #     event_handler=launch.event_handlers.OnProcessExit(
         #         target_action=livox_rviz,
